940f1963dd14b94e7adf748295fa4c4046943d0d
putz.patrick@outlook.com <putz.patrick@outlook.com>
2021-09-26 17:54:09 +0200

    implemented scanner and calculation for dollar to euro and vice versa

--- src/WaehrungsUmrechnung.java
+++ src/WaehrungsUmrechnung.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class WaehrungsUmrechnung {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double value = 0.0;
+
+        // scan for value for calculation
+        System.out.println("Welcome to the € - $ calculator!");
+        System.out.print("Enter the value you'd like to have calculated: ");
+        Scanner getValue = new Scanner(System.in);
+        value = getValue.nextDouble();
+
+        // calculate and print $ and € calculation
+        System.out.println(value + "$ = " + (value * 0.85) + "€");
+        System.out.println(value + "€ = " + (value / 85 * 100) + (" $"));
     }
 }
